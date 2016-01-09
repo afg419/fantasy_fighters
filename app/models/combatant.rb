31f3554da27466b507a9a2dce9a9d9e7f271765a
Aaron Greenspan <afg419@gmail.com>
2016-01-09 13:07:58 -0700

    Validations for combatant

--- app/models/combatant.rb
+++ app/models/combatant.rb
@@ -1,6 +1,19 @@
 class Combatant < ActiveRecord::Base
   belongs_to :dojo
 
+  validates :name, presence: true,
+                    uniqueness: { scope: :dojo_id }
+  validates :race, presence: true
+  validates :strength, presence: true,
+                      inclusion: { in: (0..15) }
+  validates :intelligence, presence: true,
+                      inclusion: { in: (0..15) }
+  validates :dexterity, presence: true,
+                      inclusion: { in: (0..15) }
+  validates :hp, presence: true
+
+
+
   def to_param
     "#{name.capitalize.gsub(" ","***")}" if name
   end
--- spec/models/combatant_spec.rb
+++ spec/models/combatant_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Combatant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:race) }
+    it { is_expected.to validate_presence_of(:strength) }
+    it { is_expected.to validate_presence_of(:intelligence) }
+    it { is_expected.to validate_presence_of(:dexterity) }
+    it { is_expected.to validate_presence_of(:hp) }
+
+    it { is_expected.to validate_inclusion_of(:strength).in_range(0..15) }
+    it { is_expected.to validate_inclusion_of(:intelligence).in_range(0..15) }
+    it { is_expected.to validate_inclusion_of(:dexterity).in_range(0..15) }
+
+    it { is_expected.to validate_uniqueness_of(:name).scoped_to(:dojo_id) }
+  end
 end
